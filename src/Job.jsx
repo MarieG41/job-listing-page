8e92025247cf8ce1472ac53cfc1d974f6c9028fc
MarieG41 <mariegond8@gmail.com>
2024-06-06 17:47:22 +0200

    component grid layout

--- src/Job.jsx
+++ src/Job.jsx
@@ -1,24 +1,47 @@
+import { useEffect, useState } from 'react'
 import datas from './data.json'
 import './job.css'
 
 export default function Job() {
+    const [ role, setRole ] = useState(false)
+    const [ datasList, setDatasList ] = useState([])
+
+    function handleRole() {
+        setRole(!role)
+        console.log('click');
+    }
+    useEffect(() => {
+        const filteredRole = role ? datas.filter((data) => data.role) : datas
+        setDatasList(filteredRole)
+    }, [role])
     return (
         <>
-            {datas.map((data) => {
+            {datasList.length > 0 && datas.map((data) => {
                 return (
-                    <div className='job-item' key={data.id}>
-                        <img src={require(`${data.logo}`)} alt={`logo de ${data.company}`} />
-                        <h2 className='title'>{data.company}</h2>
-                        <span>{data.new}</span>
-                        <span>{data.featured}</span>
-                        <h3>{data.position}</h3>
-                        <p>{data.role}</p>
-                        <p>{data.level}</p>
-                        <p className='grey'>{data.postedAt}</p>
-                        <p className='grey'>{data.location}</p>
-                        <p className='grey'>{data.contract}</p>
-                        <p>{data.languages}</p>
-                        <p>{data.tools}</p>
+                    <div className='job-item grid' key={data.id}>
+                        <div className='div-img'>
+                            <img src={require(`${data.logo}`)} alt={`logo de ${data.company}`} />
+                        </div>
+                        <div className='div-main-infos'>
+                            <div className='flex'>
+                                <h2 className='title'>{data.company}</h2>
+                                {data.new && <span className='new'>NEW!</span>}
+                                {data.featured && <span className='featured'>FEATURED</span>}
+                            </div>
+                            
+                            <h3 className='position'>{data.position}</h3>
+                            <div className='flex'>
+                                <p className='grey'>{data.postedAt}<span> . </span></p>
+                                <p className='grey'>{data.contract}<span> . </span></p>
+                                <p className='grey'>{data.location}</p>
+                            </div>
+                        </div>
+                        <div className='div-other-infos flex'>
+                            <button className='tag' onClick={handleRole}>{data.role}</button>
+                            <p className='tag'>{data.level}</p>
+                            <p className='tag'>{data.languages}</p>
+                            <p className='tag'>{data.tools}</p>
+                        </div>
                     </div>
                 )
             })}
